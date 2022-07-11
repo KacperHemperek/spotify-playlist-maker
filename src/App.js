41036e799affb5afcf51f0ffe1ca22a48e8a89b8
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-11 17:38:22 +0200

    feat: redirects user to get spotify auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Button } from "react-bootstrap";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>{" "}
-    </div>
-  );
+    const client_id = "71a0250dc8674aa0b6d49ec82695bfab";
+    const redirect = "http://localhost:3000";
+    const auth = "https://accounts.spotify.com/authorize";
+    const type = "token";
+
+    
+
+    return (
+        <div className="App text-center ">
+            <a
+                className="btn btn-primary btn-lg"
+                href={`${auth}?response_type=${type}&client_id=${client_id}&redirect_uri=${redirect}`}
+            >
+                Log in
+            </a>
+        </div>
+    );
 }
 
 export default App;
