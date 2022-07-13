e7c73c4a3409679eb507fc93ad536d60dcf39509
Kacper Hemperek <kacper.hemperek@delivence.com>
2022-07-13 18:17:18 +0200

    feat: changed the way props are passed

--- src/components/track.tsx
+++ src/components/track.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { Track } from "../App";
 
-type TrackProp ={
-  track: Track;
-}
+type TrackProp = {
+    authors: any[];
+    title: string;
+    img: string;
+    id: string;
+};
 
-
-const TrackCard = ({ track }:TrackProp) => {
-  const {authors, title, img, id} = track;
-  return (
-    <div className="track-container" key={id}>
-      <table>
-        <tr>
-          <th> <img alt={`track-${title}`} src={img} width="50" height="50"/> </th>
-          <th>{title}</th>
-          <th>{authors.join(", ")}</th>
-        </tr>
-      </table>
-    </div>
-  );
+const TrackCard = ({ authors, title, img, id }: TrackProp) => {
+    return (
+        <div className="track-container" key={id}>
+            <table>
+                <tr>
+                    <th>
+                        <img
+                            alt={`track-${title}`}
+                            src={img}
+                            width="50"
+                            height="50"
+                        />
+                    </th>
+                    <th>{title}</th>
+                    <th>{authors.join(", ")}</th>
+                </tr>
+            </table>
+        </div>
+    );
 };
 
 export default TrackCard;
